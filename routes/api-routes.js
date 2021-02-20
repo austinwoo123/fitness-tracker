882465df10d56b60cf85ae1821f8da3e3754fd62
Austin Woo <austinwoo123@gmail.com>
2021-02-19 18:57:56 -0800

    create put route in api-routes.js file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,10 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const workout = require("../models/workout.js");
 
 module.exports = function (app) {
 
-    app.get("/api/workouts", function (req, res) {
+    router.get("/api/workouts", function (req, res) {
         workout.find()
             .then(data => {
                 res.json(data)
@@ -11,6 +12,25 @@ module.exports = function (app) {
             .catch(err => {
                 res.json(err)
             })
+    })
+
+
+    router.put("/api/workouts/:id", ({ body, params }, res) => {
+        workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        )
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+
+    router.post("/api/workouts", (req, res) => {
+
     })
 
 
